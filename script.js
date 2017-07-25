8c796d69ef88677901c439af7f5cf86baf93ec0a
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-25 12:37:11 -0600

    header bar complete

--- script.js
+++ script.js
@@ -28,3 +28,22 @@ window.onload = () => {
   document.getElementById('tab-1').classList.add('show-tab');
   document.querySelector('.tab-name').classList.add('show-header');
 }
+
+const headerBar = document.getElementById('header-bar');
+
+headerBar.addEventListener('click', function() {
+  if(window.innerWidth < 840) {
+    const navTab = document.getElementById('nav-tab');
+
+    navTab.style.display = navTab.style.display === 'none' ? 'flex' : 'none';
+  }
+})
+
+window.onresize = () => {
+  const navTab = document.getElementById('nav-tab');
+  if(window.innerWidth > 840) {
+    navTab.style.display = 'flex';
+  } else {
+    navTab.style.display = 'none';
+  }
+}
